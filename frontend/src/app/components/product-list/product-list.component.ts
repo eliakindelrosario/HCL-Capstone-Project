999891a698846ed114a6ba9455ae6ea01b596cda
Eliakin Del Rosario <eking1566@gmail.com>
2022-03-04 14:46:52 -0600

    FEATURE [Frontend Cart] - new cart component, new cart service, add items to cart

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -22,6 +24,7 @@ export class ProductListComponent implements OnInit {
 
   constructor(
     private productService: ProductService,
+    private cartService: CartService,
     private route: ActivatedRoute
   ) {}
 
@@ -100,5 +103,11 @@ export class ProductListComponent implements OnInit {
     this.pageSize = userPageSize;
     this.pageNumber = 1;
     this.listProducts();
+  }
+
+  addToCart(product: Product) {
+    console.log(`Adding ${product.name} ${product.unitPrice} to cart.`);
+    const cartItem = new CartItem(product);
+    this.cartService.addToCart(cartItem);
   }
 }
