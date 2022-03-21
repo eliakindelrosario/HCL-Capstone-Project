ef784e0b9ac347873e22ddda41adbd6700391f6e
Eliakin Del Rosario <eking1566@gmail.com>
2022-03-21 15:11:58 -0500

    Secure Communication update

--- frontend/src/app/config/my-app-config.ts
+++ frontend/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oa448vv91flblgPW5d7',
     issuer: 'https://dev-4073510.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
