2f352777f497e9bcc96c88312987ef878b4d1984
Eliakin Del Rosario <eking1566@gmail.com>
2022-03-22 17:27:29 -0500

    Backend Update - Stripe Payment

--- backend/src/main/java/com/eliakin/ecommerce/controller/CheckoutController.java
+++ backend/src/main/java/com/eliakin/ecommerce/controller/CheckoutController.java
@@ -1,15 +1,20 @@
 package com.eliakin.ecommerce.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eliakin.ecommerce.dto.PaymentInfo;
 import com.eliakin.ecommerce.dto.Purchase;
 import com.eliakin.ecommerce.dto.PurchaseResponse;
 import com.eliakin.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 @CrossOrigin("http://localhost:4200")
 @RestController
@@ -29,6 +34,17 @@ public class CheckoutController {
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
         return purchaseResponse;
+    }
+    
+    
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 
 }
