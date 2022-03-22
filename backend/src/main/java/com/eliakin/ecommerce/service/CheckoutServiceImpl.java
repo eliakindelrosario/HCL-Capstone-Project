fc22810ee85290ced3604d17cad8fa730ac3ee61
Eliakin Del Rosario <eking1566@gmail.com>
2022-03-22 14:35:56 -0500

    STRIPE setup backend

--- backend/src/main/java/com/eliakin/ecommerce/service/CheckoutServiceImpl.java
+++ backend/src/main/java/com/eliakin/ecommerce/service/CheckoutServiceImpl.java
@@ -1,26 +1,38 @@
 package com.eliakin.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.eliakin.ecommerce.dao.CustomerRepository;
+import com.eliakin.ecommerce.dto.PaymentInfo;
 import com.eliakin.ecommerce.dto.Purchase;
 import com.eliakin.ecommerce.dto.PurchaseResponse;
 import com.eliakin.ecommerce.entity.Customer;
 import com.eliakin.ecommerce.entity.Order;
 import com.eliakin.ecommerce.entity.OrderItem;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
 
 	private CustomerRepository customerRepository;
 
-	public CheckoutServiceImpl(CustomerRepository customerRepository) {
+	public CheckoutServiceImpl(CustomerRepository customerRepository, @Value("${stripe.key.secret}") String secretKey) {
 		this.customerRepository = customerRepository;
+
+		// Initialize Stripe Api
+		Stripe.apiKey = secretKey;
 	}
 
 	@Override
@@ -68,5 +80,18 @@ public class CheckoutServiceImpl implements CheckoutService {
 
 		// generate a random UUID number (UUID version-4)
 		return UUID.randomUUID().toString();
+	}
+
+	@Override
+	public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+		List<String> paymentMethodTypes = new ArrayList<>();
+		paymentMethodTypes.add("card");
+
+		Map<String, Object> params = new HashMap<>();
+		params.put("amount", paymentInfo.getAmount());
+		params.put("currency", paymentInfo.getCurrency());
+		params.put("payment_method_types", paymentMethodTypes);
+
+		return PaymentIntent.create(params);
 	}
 }
